6664f35a9e65c821a06a7ad45c2f33e7eb6e36db
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-02-10 19:00:41 +0300

    Update 2.3.py

--- №1/2.3.py
+++ №1/2.3.py
@@ -1,1 +1,7 @@
-
+x = int(input("Введите число: "))
+if x <= -5:
+    print ("Принадлежит  (-infinity, -5)")
+elif x >= 5:
+    print("Принадлежит (5, +infinity)")
+else:
+    print("Принадлежит [-5, 5]")
