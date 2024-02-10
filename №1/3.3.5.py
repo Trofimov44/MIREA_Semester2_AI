a6cd7a991dc02945a8c1a22788db0d02bc534524
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-02-10 19:06:46 +0300

    Update 3.3.5.py

--- №1/3.3.5.py
+++ №1/3.3.5.py
@@ -1,1 +1,15 @@
-
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+a = x[1::2]
+b = x[0::2]
+b = b[::-1]
+q = []
+print(a)
+print(b)
+if len(b) > len(a):
+    c = len(b)
+else:
+    c = len(a)
+for i in range(0, c):
+    q.append(b[i])
+    q.append(a[i])
+print(q)
