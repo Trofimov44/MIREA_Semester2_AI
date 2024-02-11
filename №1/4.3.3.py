2d87546193b7e17e5c45bd1706462b639cee8cc4
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-02-11 18:04:59 +0300

    Update 4.3.3.py

--- №1/4.3.3.py
+++ №1/4.3.3.py
@@ -1,1 +1,15 @@
+from scipy.integrate import simps
+from numpy import trapz
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
 
+x = np.arange(0.0,10,1)
+y = np.abs(np.cos(x*np.e**(np.cos(x)+np.log(x+1))))
+
+plt.grid(True)
+plt.plot(x,y, color = 'r')
+plt.fill_between(x,y)
+area = trapz(y)
+print(area)
+plt.show()
