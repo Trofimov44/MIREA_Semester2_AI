1f15933c67707a2cf2ba7c6ca8d2c173407bdf4b
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-02-23 18:43:13 +0300

    Update 2.3.1.py

--- №2/2.3.1.py
+++ №2/2.3.1.py
@@ -1,1 +1,7 @@
-
+import numpy as np
+import pandas as pd
+point_1 = pd.array((0, 0, 0))
+point_2 = pd.array((2, 3, 3))
+square = np.square(point_1 - point_2)
+distance = np.sum(square)
+print(distance)
