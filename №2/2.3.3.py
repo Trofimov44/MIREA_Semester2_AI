80a4ac117d44f420f13bdb324ca5ddc872e0073e
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-03-08 14:34:15 +0300

    Update 2.3.3.py

--- №2/2.3.3.py
+++ №2/2.3.3.py
@@ -1,1 +1,10 @@
+import pandas as pd  
+u = "https://github.com/akmand/datasets/blob/main/cdc.csv" 
+dataframe=pd.read_csv(u) 
+dataframe.head(10) 
+dataframe.tail(3)  
+dataframe.shape  
+dataframe.describe()  
+dataframe.iloc[1:4]  
+dataframe[dataframe[''] == ''].head(5)
 
