f0adf2c04195ace8ddd4fe536beeb6f96bedcee6
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-03-08 15:05:44 +0300

    Update 3.3.2.py

--- №2/3.3.2.py
+++ №2/3.3.2.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+url = "https://raw.githubusercontent.com/akmand/datasets/master/iris.csv"
+df = pd.read_csv(url)
+df['sepal_length_cm_normalized'] = MinMaxScaler().fit_transform(df[['sepal_length_cm']])
+df['sepal_width_cm_standardized'] = StandardScaler().fit_transform(df[['sepal_width_cm']])
+print(df.head())
+
 
