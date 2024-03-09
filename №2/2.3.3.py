5c66814b53b6f6c0355a46208e85f449358cd3dc
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-03-09 13:01:51 +0300

    Update 2.3.3.py

--- №2/2.3.3.py
+++ №2/2.3.3.py
@@ -1,9 +1,9 @@
 import pandas as pd  
-u = "" 
-dataframe=pd.read_csv(u) 
-dataframe.head(10) 
-dataframe.tail(3)  
-dataframe.shape  
-dataframe.describe()  
-dataframe.iloc[1:4]  
-dataframe[dataframe['species'] == 'setosa'].head()
+url = "https://raw.githubusercontent.com/akmand/datasets/master/iris.csv"
+df = pd.read_csv(url)
+df.head(10) 
+df.tail(3)  
+df.shape  
+df.describe()  
+df.iloc[1:4]  
+df[df['species'] == 'setosa'].head()
